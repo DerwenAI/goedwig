27927bf14a1ef4b8b751669f5e0f9d06e0132f63
Paco Nathan <ceteri@gmail.com>
2021-10-10 10:21:46 -0700

    fix versioning bug

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,12 @@
 # `goedwig` changelog
 
+## 0.1.2
+
+2021-10-10
+
+  * fix versioning bug
+
+
 ## 0.1.1
 
 2021-10-10
--- goedwig/version.py
+++ goedwig/version.py
@@ -13,6 +13,6 @@ from git import Repo  # type: ignore  # pylint: disable=E0401
 
 repo_path = pathlib.Path(dirname(__file__))
 repo = Repo(repo_path.parents[0])
-__version__ = str(repo.tags[0])
+__version__ = str(repo.tags[-1])
 
 MIN_PY_VERSION: typing.Tuple = (3, 8,)
