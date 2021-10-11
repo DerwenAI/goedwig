60175680413795caee80db6b548c293021d90870
Paco Nathan <ceteri@gmail.com>
2021-10-11 14:38:08 -0700

    parsing paths with relations

--- goedwig/query.py
+++ goedwig/query.py
@@ -13,9 +13,9 @@ from icecream import ic  # type: ignore  # pylint: disable=E0401,W0611
 from .cypher import CypherItem
 
 
-class NodeVariable:  # pylint: disable=R0903
+class Variable:  # pylint: disable=R0903
     """
-Represent a node variable within a query.
+Represent a node or edge variable within a query.
     """
     def __init__ (
         self,
@@ -27,6 +27,7 @@ Constructor.
         """
         self.name: str = name
         self.item: int = item
+        self.predicates: typing.List[Predicate] = []
 
 
     def __repr__ (
@@ -38,25 +39,40 @@ Text representation.
         _repr = {
             "name": self.name,
             "item": self.item,
+            "predicates": self.predicates,
         }
 
         return str(_repr)
 
 
-class PredicateMap:  # pylint: disable=R0903
+class Predicate:  # pylint: disable=R0903
+    """
+Represent a simple predicate.
+    """
+    def __init__ (
+        self,
+        var: Variable,
+        ) -> None:
+        """
+Constructor.
+        """
+        self.var: Variable = var
+
+
+class PredicateMap (Predicate):  # pylint: disable=R0903
     """
 Represent a predicate that maps a property key/value pair onto a variable.
     """
     def __init__ (
         self,
-        var: typing.Any,
+        var: Variable,
         key: str,
         val: str,
         ) -> None:
         """
 Constructor.
         """
-        self.var: typing.Any = var
+        super().__init__(var)
         self.key: str = key
         self.val: str = val
 
@@ -68,7 +84,7 @@ Constructor.
 Text representation.
         """
         _repr = {
-            "name": self.var.name,
+            "kind": "map",
             "key": self.key,
             "val": self.val,
         }
@@ -76,20 +92,20 @@ Text representation.
         return str(_repr)
 
 
-class ProjectionElement:  # pylint: disable=R0903
+class PredicateLabel (Predicate):  # pylint: disable=R0903
     """
-Represent an element of the returned projection.
+Represent a predicate that specifies a label.
     """
     def __init__ (
         self,
-        name: str,
-        prop: typing.Optional[str],
+        var: Variable,
+        label: str,
         ) -> None:
         """
 Constructor.
         """
-        self.name: str = name
-        self.prop: typing.Optional[str] = prop
+        super().__init__(var)
+        self.label: str = label
 
 
     def __repr__ (
@@ -99,8 +115,93 @@ Constructor.
 Text representation.
         """
         _repr = {
-            "name": self.name,
+            "kind": "label",
+            "label": self.label,
+        }
+
+        return str(_repr)
+
+
+class PredicateDirection (Predicate):  # pylint: disable=R0903
+    """
+Represent a predicate that specifies a direction.
+    """
+    def __init__ (
+        self,
+        var: Variable,
+        direction: str,
+        ) -> None:
+        """
+Constructor.
+        """
+        super().__init__(var)
+        self.direction: str = direction
+
+
+    def __repr__ (
+        self,
+        ) -> str:
+        """
+Text representation.
+        """
+        _repr = {
+            "kind": "direction",
+            "dir": self.direction,
+        }
+
+        return str(_repr)
+
+
+class Path:  # pylint: disable=R0903
+    """
+Represent a path pattern, with multiple node/edge/regex elements.
+    """
+    def __init__ (
+        self,
+        ) -> None:
+        """
+Constructor.
+        """
+        self.elem: list = []
+
+
+    def __repr__ (
+        self,
+        ) -> str:
+        """
+Text representation.
+        """
+        return str(self.elem)
+
+
+class ProjectionElement:  # pylint: disable=R0903
+    """
+Represent an element of the returned projection.
+    """
+    def __init__ (
+        self,
+        bind: typing.Optional[str],
+        prop: typing.Optional[str],
+        alias: typing.Optional[str],
+        ) -> None:
+        """
+Constructor.
+        """
+        self.bind: typing.Optional[str] = bind
+        self.prop: typing.Optional[str] = prop
+        self.alias: typing.Optional[str] = alias
+
+
+    def __repr__ (
+        self,
+        ) -> str:
+        """
+Text representation.
+        """
+        _repr = {
+            "bind": self.bind,
             "prop": self.prop,
+            "alias": self.alias,
         }
 
         return str(_repr)
@@ -117,8 +218,10 @@ Manage the lifecycle of a query plan.
 Constructor.
         """
         self.items: typing.List[CypherItem] = []
-        self.bindings: typing.Dict[str, typing.Any] = {}
-        self.predicates: typing.List[PredicateMap] = []
+        self.blank_count: int = 0
+
+        self.bindings: typing.Dict[str, Variable] = {}
+        self.paths: typing.List[Path] = []
         self.projections: typing.List[ProjectionElement] = []
 
 
@@ -130,7 +233,7 @@ Text representation.
         """
         _repr = {
             "bindings": self.bindings,
-            "predicates": self.predicates,
+            "paths": self.paths,
             "projections": self.projections,
         }
 
@@ -158,16 +261,35 @@ Load a TSV file of AST items.
         return self
 
 
-    def parse_items (
+    def blankify (
+        self,
+        ident_lit: str,
+        ) -> str:
+        """
+Represent a unique name for a blank node or edge.
+        """
+        if len(ident_lit) < 1:
+            ident_lit = "blank_{}".format(self.blank_count)
+            self.blank_count += 1
+
+        return ident_lit
+
+
+    def parse_items (  # pylint: disable=R0912,R0914,R0915
         self,
         *,
         i: int = 0,
-        ) -> None:
+        debug: bool = False,
+        ) -> typing.Optional[typing.Any]:
         """
-Develop a query plan from the given AST items, traversed using
-recursive descent.
+Develop a query plan from the given AST items, using recursive
+descent to traverse them.
         """
         typestr = self.items[i].ast_typestr
+
+        if debug:
+            ic(i, typestr)
+            print("", self.items[i].children)
 
         if typestr in ["line_comment"]:
             # ignore
@@ -179,50 +301,118 @@ recursive descent.
                 match_i, return_i = self.items[i + 1].children
 
                 for j in self.items[match_i].children:
-                    self.parse_items(i = j)
+                    self.parse_items(i = j, debug=debug)
 
                 for j in self.items[return_i].children:
-                    self.parse_items(i = j)
+                    self.parse_items(i = j, debug=debug)
 
         elif typestr == "pattern":
-            # begin building a new predicate
+            # begin building a new predicate @ child
             pat_i = self.items[i].children[0]
-            self.parse_items(i = pat_i)
+            self.parse_items(i = pat_i, debug=debug)
 
         elif typestr == "pattern path":
-            # HERE: instantiate a triple/pattern and pass into recursion
-            pat_path_i = self.items[i].children[0]
-            self.parse_items(i = pat_path_i)
+            # populate a path pattern
+            path = Path()
+            self.paths.append(path)
+
+            for j in self.items[i].children:
+                child_type = self.items[j].ast_typestr
+
+                if child_type == "node pattern":
+                    path.elem.append(self.parse_items(i = j, debug=debug))
+
+                elif child_type == "rel pattern":
+                    path.elem.append(self.parse_items(i = j, debug=debug))
+
+            return path
+
 
         elif typestr == "node pattern":
             # represent a node pattern
-            ident_i, map_i = self.items[i].children
+            ident_i = self.items[i].children[0]
+            ident_lit = self.blankify(self.items[ident_i].literal)
 
-            node_var = NodeVariable(
-                self.items[ident_i].literal,  # pylint: disable=W0621
-                map_i,
-                )
-
-            self.bindings[node_var.name] = node_var
+            node_var = Variable(ident_lit, i)
+            self.bindings[ident_lit] = node_var
 
             ## selection
-            prop_i, val_i = self.items[map_i].children
+            for j in self.items[i].children[1:]:
+                child_type = self.items[j].ast_typestr
 
-            pred = PredicateMap(
-                node_var,
-                self.items[prop_i].literal,  # pylint: disable=W0621
-                self.items[val_i].literal.strip("'"),  # pylint: disable=W0621
-            )
+                if child_type == "label":
+                    node_var.predicates.append(PredicateLabel(
+                        node_var,
+                        self.items[j].literal.strip(":"),
+                    ))
 
-            self.predicates.append(pred)
+                elif child_type == "map":
+                    key_i, val_i = self.items[j].children
+
+                    node_var.predicates.append(PredicateMap(
+                        node_var,
+                        self.items[key_i].literal,  # pylint: disable=W0621
+                        self.items[val_i].literal.strip("'"),  # pylint: disable=W0621
+                    ))
+
+            return node_var
+
+        elif typestr == "rel pattern":
+            # represent a rel pattern
+            ident_i = self.items[i].children[0]
+            ident_lit = self.blankify(self.items[ident_i].literal)
+
+            edge_var = Variable(ident_lit, i)
+            self.bindings[ident_lit] = edge_var
+
+            ## selection
+            for j in self.items[i].children[1:]:
+                child_type = self.items[j].ast_typestr
+
+                if child_type == "label":
+                    edge_var.predicates.append(PredicateLabel(
+                        edge_var,
+                        self.items[j].literal.strip(":"),
+                    ))
+
+                elif child_type == "map":
+                    key_i, val_i = self.items[j].children
+
+                    edge_var.predicates.append(PredicateMap(
+                        edge_var,
+                        self.items[key_i].literal,  # pylint: disable=W0621
+                        self.items[val_i].literal.strip("'"),  # pylint: disable=W0621
+                    ))
+
+                elif child_type == "rel type":
+                    edge_var.predicates.append(PredicateDirection(
+                        edge_var,
+                        self.items[j].literal.strip(":"),
+                    ))
+
+            return edge_var
 
         elif typestr == "projection":
             ## projection
-            ident_i = self.items[i].children[0]
+            bind_lit = None
+            prop_lit = None
+            alias_lit = None
 
-            proj_elem = ProjectionElement(
-                self.items[ident_i].literal,  # pylint: disable=W0621
-                None,
-                )
+            for j in self.items[i].children:
+                child_type = self.items[j].ast_typestr
 
-            self.projections.append(proj_elem)
+                if child_type == "identifier":
+                    alias_lit = self.items[j].literal.strip("`")
+
+                elif child_type == "property":
+                    ident_i, prop_i = self.items[j].children
+                    bind_lit = self.items[ident_i].literal
+                    prop_lit = self.items[prop_i].literal
+
+            if not bind_lit:
+                # direct reference; no alias given
+                bind_lit = alias_lit
+
+            self.projections.append(ProjectionElement(bind_lit, prop_lit, alias_lit))
+
+        return None
--- sample.py
+++ sample.py
@@ -5,26 +5,39 @@
 Sample code.
 """
 
+import json
 import pathlib
+import sys
 
 from icecream import ic  # type: ignore  # pylint: disable=E0401,W0611
 import goedwig
 
 
 if __name__ == "__main__":
-    print(goedwig.__version__)
+    filename = "q1.tsv"  # pylint: disable=C0103
+    testing = False  # pylint: disable=C0103
 
-    ast_path = pathlib.Path("dat/cyp") / "q1.tsv"
+    if len(sys.argv) > 1:
+        filename = sys.argv[1]
 
+    # load the parsed AST trees
     q = goedwig.QueryPlan()
+    ast_path = pathlib.Path("dat/cyp") / filename
     tsv_iter = iter(ast_path.open(encoding="utf-8").readlines())
     q.load_ast(tsv_iter)
+
+    # print a representation
+    print(goedwig.__version__)
 
     for item in q.items:
         print(item)
 
-    q.parse_items()
-    print(q)
+    # define a query plan
+    q.parse_items(debug=True)
+    plan = eval(str(q))  # pylint: disable=W0123
+    print(json.dumps(plan, indent=1, sort_keys=False))
 
-    assert len(q.items) == 14
-    assert q.items[-1].literal == "p"
+    # testing assertions
+    if testing:
+        assert len(q.items) == 14
+        assert q.items[-1].literal == "p"
